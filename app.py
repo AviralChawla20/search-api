4c7e58c4573337a149d080d5265b958b1a57c2d5
AviralChawla20 <aviralchawla02@gmail.com.com>
2023-11-24 18:52:41 +0530

    updated port

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ if __name__ == "__main__":
     import uvicorn
 
     # Run the application using Uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8081)
